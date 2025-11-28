375a73c53b3ba8cd2604758bdf49710ec8c3886d
ajedrez <vargasjalinson@icloud.com>
2025-11-28 10:47:40 -0500

    corrected vite file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/graficos_backup/", // <---- ESTA LÍNEA ES LA CLAVE
+  base: "/", // ✔️ Netlify necesita esto
   plugins: [react()],
 });
